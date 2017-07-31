5008404975acf9a084f40c0a116e55156cc4495e
Gary Gilbert <gjgilbert@gmail.com>
2017-07-31 12:41:58 -0400

    v 1.0.4 of event lib

--- src/main/java/unicon/matthews/caliper/Group.java
+++ src/main/java/unicon/matthews/caliper/Group.java
@@ -13,6 +13,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
@@ -21,6 +22,7 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
  * @author ggilbert
  *
  */
+@JsonIgnoreProperties(ignoreUnknown=true)
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 @JsonDeserialize(builder = Group.Builder.class)
 public class Group implements Serializable {
